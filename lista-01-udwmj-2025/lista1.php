08e742dc717567bb2a00a5e09a6d301790563dad
Eduardo de Melo Vasconcelos <107006410+duduzinhodc@users.noreply.github.com>
2025-09-01 20:37:24 -0300

    Add files via upload

---
+++ lista-01-udwmj-2025/lista1.php
@@ -0,0 +1,17 @@
+<?php
+// Definindo as variáveis
+$num1 = 10;
+$num2 = 5;
+
+// Operações básicas
+$soma = $num1 + $num2;
+$subtracao = $num1 - $num2;
+$multiplicacao = $num1 * $num2;
+$divisao = $num1 / $num2;
+
+// Mostrando os resultados
+echo "Soma: $num1 + $num2 = $soma<br>";
+echo "Subtração: $num1 - $num2 = $subtracao<br>";
+echo "Multiplicação: $num1 * $num2 = $multiplicacao<br>";
+echo "Divisão: $num1 / $num2 = $divisao<br>";
+?>
